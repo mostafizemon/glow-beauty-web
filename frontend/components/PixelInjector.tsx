bd495638c433b7d34ae3ca86c62a3e5c4cc1c7b3
naturalglow404 <naturalglow404@gmail.com>
2026-04-27 00:44:52 +0600

    update: tiktok browser script base initialization

--- frontend/components/PixelInjector.tsx
+++ frontend/components/PixelInjector.tsx
@@ -42,6 +42,12 @@ export default function PixelInjector() {
         const metaTest = res.data?.meta_test_code || "";
         const tiktokTest = res.data?.tiktok_test_code || "";
         
+        console.log("[Pixels] Loaded configuration:", {
+          meta: metaId ? "Present" : "Missing",
+          tiktok: tiktokId ? "Present" : "Missing",
+          apiSource: "database"
+        });
+
         setPixels({
           meta: metaId,
           tiktok: tiktokId,
@@ -50,7 +56,7 @@ export default function PixelInjector() {
         });
       })
       .catch((err) => {
-        console.error("Failed to fetch pixel settings, using env fallbacks", err);
+        console.error("[Pixels] Failed to fetch settings from API, using env fallbacks:", err);
         setPixels({
           meta: process.env.NEXT_PUBLIC_META_PIXEL_ID || "",
           tiktok: process.env.NEXT_PUBLIC_TIKTOK_PIXEL_ID || "",
@@ -95,6 +101,7 @@ export default function PixelInjector() {
               !function (w, d, t) {
                 w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=i+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
                 ttq.load('${pixels.tiktok}');
+                ttq.page();
               }(window, document, 'ttq');
             `,
           }}
